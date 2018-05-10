3006b101c984bf6d401a8b2cf97e1f25767fc7da
Sagar Ladla <sagarladla10@gmail.com>
2018-05-10 17:55:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import tweepy
-form textblob imort TextBlob
+from textblob import TextBlob
 
 consumer_key = input("Enter consumer key : ")
 consumer_secret = input("Enter consumer secret : ")
@@ -11,6 +11,7 @@ access_token_secret = input("Enter access token secret : ")
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth = auth.set_access_token(access_token, access_token_secret)
 
+#Use twitter API to get all the access to twitter's data 
 api = tweepy.API(auth)
 
 #taking input of interested topic
@@ -19,9 +20,9 @@ topic = input("Enter your interested topic : ")
 #fetching all tweets related to some input topic
 public_tweets = api.search(topic)
 
+#printing all tweets as text and their respective sentiments
 for tweet in public_tweets:
 	print(tweet.text)
 	analysis = TextBlob(tweet.text)
 	sentiment_analysis = analysis.sentiment
 	print(sentiment_analysis)
-	
